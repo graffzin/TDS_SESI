fd811e8c5062b254eb8805c3344e668e848b39fb
graffzin <gustavograffreiszanon27@gmail.com>
2026-02-26 12:13:03 -0300

    inicio com JS

---
+++ inicio/comentarios.js
@@ -0,0 +1,12 @@
+// O console é utilizado para imprimir algo na tela
+
+console.log("sesi");
+console.log("senai");
+
+
+/* 
+varias
+linhas
+de
+comentarios
+*/
---
+++ inicio/desafio_bloco.js
@@ -0,0 +1,21 @@
+// desafio: criar dois blocos de codigo onde o primeiro deve imprimir na tela os numeros pares de 0 a 10 e o segundo bloco deve imprimir na tela os impares
+
+console.log("2 4 6 8 10");
+
+console.log("1 3 5 7 9");
+
+{
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10");
+}
+
+{
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
+}
