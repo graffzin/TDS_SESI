c151a7211b87bc8b5ffc365b433714ad343c738b
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-06 12:12:41 -0300

    Operadores logicos, unarios e ternarios

---
+++ operadores/logicos.js
@@ -0,0 +1,23 @@
+let temDinheiro = true;
+let estaChovendo = false;
+let carroEstaNaGaragem = false;
+
+let resultadoAND = "#AND Voce vai ao shopping? ";
+resultadoAND += temDinheiro && estaChovendo;
+console.log(resultadoAND);
+
+let resultadoOR = "#OR Voce vai ao shopping? ";
+resultadoOR += temDinheiro || carroEstaNaGaragem;
+console.log(resultadoOR);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operadores/ternario.js
@@ -0,0 +1,10 @@
+const hora = 13;
+let mensagem = hora <= 12 ? "Bom dia" : "Boa tarde";
+console.log(mensagem);
+
+const hora2 = 12;
+let mensagem2 = hora2 <= 12 ? "Bom dia" : "Boa tarde";
+console.log(mensagem2);
+
+
+
