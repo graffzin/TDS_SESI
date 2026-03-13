8e0597aa8bd1f35e7b32446694b6a8685ec1adbe
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-13 12:15:57 -0300

    Array

---
+++ array/array2.js
@@ -0,0 +1,11 @@
+// Desafio: Inserir 5 novos indices com 5 nomes e imprimir na tela
+
+const nomes = [];
+
+nomes[0] = "Gustavo"
+nomes[1] = "Angelo"
+nomes[2] = "Zoio"
+nomes[3] = "Joao"
+nomes[4] = "Alek"
+
+console.log(nomes);
---
+++ array/desafio_array.js
@@ -0,0 +1,18 @@
+
+// Inserir 10 numeros utilizando a funcao .push() ;
+// e imprimir na tela utilizando o formato: "Numero: X" ;
+
+const numeros = [];
+
+numeros.push(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+
+console.log("Numero: " + numeros[0]);
+console.log("Numero: " + numeros[1]);
+console.log("Numero: " + numeros[2]);
+console.log("Numero: " + numeros[3]);
+console.log("Numero: " + numeros[4]);
+console.log("Numero: " + numeros[5]);
+console.log("Numero: " + numeros[6]);
+console.log("Numero: " + numeros[7]);
+console.log("Numero: " + numeros[8]);
+console.log("Numero: " + numeros[9]);
