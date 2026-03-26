a66cfd6bf32ac844df5097faf4ea64a7dd61d377
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-26 12:07:30 -0300

    atividade 3

---
+++ atividades/atividade3.js
@@ -0,0 +1,22 @@
+/* 
+
+a) crie uma logica que verifique se um usuario possui acesso a um evento VIP
+
+b) condicao: o usuario deve ter idade superior a 18 && (E) possuir um convite (possui convite = true)
+
+c) exiba uma mensagem de autorizacao baseada no resultado logico
+
+*/
+
+let possuiConvite = true;
+var idade = 18;
+
+if (idade >= 18 && possuiConvite == true)
+{
+    console.log("Voce tem um convite e idade igual ou superior a 18! Voce esta liberado!")
+}
+
+else 
+{
+    console.log("Voce nao tem um convite e idade igual ou superior a 18! Voce nao esta liberado!")
+}
