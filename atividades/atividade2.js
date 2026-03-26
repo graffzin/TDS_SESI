981e70b170c8c0587601b46a6946c4899e622345
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-26 11:43:59 -0300

    atividade 2

---
+++ atividades/atividade2.js
@@ -0,0 +1,14 @@
+/* 
+
+a) declare uma variavel para a idade real de um cachorro
+
+b) calcule a idade equivalente em anos humanos (multiplicado por 7)
+
+c) armazene o resultado em uma nova variavel e exiba uma mensagem explicativa
+
+*/
+
+var idadeCachorro = 5
+var idadeHumanaDoCachorro = idadeCachorro * 7
+
+console.log(`O cachorro tem ${idadeCachorro} anos de idade e a idade humana dele, (multiplicada por 7) é ${idadeHumanaDoCachorro}.`);
