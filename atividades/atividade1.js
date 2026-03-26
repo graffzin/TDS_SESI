3e05e2ebd05bc9338cacdcb0e12e733e61c16af2
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-26 11:16:27 -0300

    atividade 1

---
+++ atividades/atividade1.js
@@ -0,0 +1,13 @@
+/* 
+a) declare variaveis para armazenar o nome de um produto (string), o preco unitario (number) e se ele esta em estoque (boolean)
+
+b) exiba o resultado no console utilizando uma unica string formatada com template string
+*/
+
+let produto = "Notebook";
+const preco = 3500;
+let estoque = true;
+
+console.log(typeof produto);
+console.log(typeof preco);
+console.log(typeof estoque);
