c71aa581ae1972f44a8a9bcc2c6bc4b8bb0e5b9f
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-27 11:30:08 -0300

    atividade 5

---
+++ atividades/atividade5.js
@@ -0,0 +1,13 @@
+/* 
+
+a) declare uma variavel estaLigado como true
+
+b) utilize o operador unario de negacao (!) para inverter o valor da variavel e exibi-la novamente
+
+*/
+
+let estaLigado = true;
+let estaLigado2 =! true;
+
+console.log(estaLigado);
+console.log(estaLigado2);
