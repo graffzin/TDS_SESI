fbf7d04cccc21db8dd532f20a4be34f2836528ca
graffzin <gustavograffreiszanon27@gmail.com>
2026-03-27 12:07:41 -0300

    diferentes versoes da atividade 7

---
+++ atividades/atividade7.js
@@ -0,0 +1,15 @@
+/* 
+
+a) simule um processo de conferencia de mercadorias no backend
+
+b) crie um loop (for, while) que conte de 1 a 10
+
+c) a cada iteracao, o console deve exibir: "item #(X) verificado e registrado no inventario"
+
+*/
+
+for (let mercadoria = 1; mercadoria <= 10; mercadoria++)
+{
+    console.log(`Item #${mercadoria} verificado e registrado no inventario. `);
+}
+console.log("Fim");
