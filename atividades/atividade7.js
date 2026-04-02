4fe03ce8d812455db1de9bd23497ba82c00a86f8
graffzin <gustavograffreiszanon27@gmail.com>
2026-04-02 11:54:38 -0300

    atividade7.js

--- atividades/atividade7.js
+++ atividades/atividade7.js
@@ -10,6 +10,6 @@ c) a cada iteracao, o console deve exibir: "item #(X) verificado e registrado no
 
 for (let mercadoria = 1; mercadoria <= 10; mercadoria++)
 {
-    console.log(`Item #${mercadoria} verificado e registrado no inventario. `);
+    console.log (`Item #${mercadoria} verificado e registrado no inventario. `);
 }
-console.log("Fim");
+console.log ("Fim");
