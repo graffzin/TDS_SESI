3fc499d53f5e9535a88ccc9f9a81ca14a0fa391d
graffzin <gustavograffreiszanon27@gmail.com>
2026-04-02 11:47:09 -0300

    atividade10

---
+++ atividades/atividade10.js
@@ -0,0 +1,17 @@
+/* 
+
+a) crie um array com 5 nomes de itens de hardware
+
+b) adicione um novo item ao final da lista utilizando obrigatoriamente o metodo .push()
+
+c) remova o primeiro item da lista utilizando obrigatoriamente o metodo shift()
+
+d) exiba o estado final da lista
+
+*/
+
+const hardwares = ["Placa de video", "Placa mae", "Fonte", "Processador", "Gabinete"];
+
+hardwares.push("SSD");
+hardwares.shift("Placa de video");
+console.log(hardwares);
