b32a7d9a3e987745a4d362ac9b0257012b0b4660
graffzin <gustavograffreiszanon27@gmail.com>
2026-04-02 12:13:52 -0300

    atividade11.js

---
+++ atividades/atividade11.js
@@ -0,0 +1,21 @@
+/* 
+
+a) crie uma lista(array) com 10 nomes de alunos
+
+b) escreva um codigo que verifique se o nome "Lucas" esta na lista
+
+c) exiba: "Usuario encontrado no sistema" ou "Usuario nao localizado"
+
+*/
+
+const nomesAlunos = ["Gustavo", "Angelo", "Henrico", "Joao", "Alex", "Zoio", "Felipe", "Pedro", "Saymon", "Kelven"];
+
+if (nomesAlunos == "Lucas")
+{
+    console.log("Voce ta na lista Luquinhas ");
+}
+
+else
+{  
+    console.log("Voce nao ta na lista Luquinhas ");
+}
